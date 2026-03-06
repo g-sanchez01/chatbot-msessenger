fd243a2e958f46d04219648ef1fcd800315af950
g-sanchez01 <g.sanchez@settepi.com>
2026-03-06 16:29:03 -0600

    Update flowService.js

--- services/flowService.js
+++ services/flowService.js
@@ -3,32 +3,25 @@ import { extractData } from "../models/stateModel.js"
 
 export const processMessage = async (psid, message) => {
 
-    const data = extractData(message)
-
     const leadActual = getLead(psid) || {
         nombre: null,
         telefono: null,
         localidad: null
     }
 
+    // extraer datos del mensaje
+    const data = extractData(message)
+
+    // actualizar memoria
     const leadActualizado = updateLead(psid, {
         nombre: data.nombre || leadActual.nombre,
         telefono: data.telefono || leadActual.telefono,
         localidad: data.localidad || leadActual.localidad
     })
 
-    // Si ya tenemos todos los datos, guardar en sheets
-    if (
-        leadActualizado.nombre &&
-        leadActualizado.telefono &&
-        leadActualizado.localidad
-    ) {
-        await saveLead(
-            psid,
-            leadActualizado.nombre,
-            leadActualizado.localidad,
-            leadActualizado.telefono
-        )
+    // si ya tiene todos los datos, guardar en Sheets
+    if (leadActualizado.nombre && leadActualizado.telefono && leadActualizado.localidad) {
+        await saveLead(psid) // <- solo psid, no más argumentos
     }
 
     console.log("Lead Actualizado:", {
