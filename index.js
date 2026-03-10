0fb74a6523ae76b13033bf6d4c3b69407c96013f
g-sanchez01 <g.sanchez@settepi.com>
2026-03-10 15:12:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import webhookRoutes from './routes/webhookRoutes.js'
-
-// Firestore
-import { initializeApp, cert } from 'firebase-admin/app'
-import { getFirestore } from 'firebase-admin/firestore'
-import serviceAccount from './serviceAccountKey.json' assert { type: 'json' }
-
 // Cargar variables de entorno desde .env
 dotenv.config()
-
-// Inicializar Firebase Admin
-initializeApp({
-  credential: cert(serviceAccount)
-})
-export const db = getFirestore()
 
 // Crear la app de Express
 const app = express()
